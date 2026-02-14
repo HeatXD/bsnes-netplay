b7304334e697bcb9bdaed5d2f99091e81cb714ac
Akiteru <akiteru@live.com>
2026-02-14 15:55:01 -0330

    CX4 cache and bus access timing fixes - Step 1+WS on both reads for cache, not just one - Remove extra cycle on bus reads/writes

--- bsnes/processor/hg51b/hg51b.cpp
+++ bsnes/processor/hg51b/hg51b.cpp
@@ -89,6 +89,7 @@ auto HG51B::cache() -> bool {
   for(uint offset : range(256)) {
     step(wait(address));
     programRAM[io.cache.page][offset]  = read(address++) << 0;
+    step(wait(address));
     programRAM[io.cache.page][offset] |= read(address++) << 8;
   }
   return io.cache.enable = 0, true;
